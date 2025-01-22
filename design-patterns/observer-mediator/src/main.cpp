36217df3842a1795a8ac15f85b31231ca83478d7
Adel Fawzy <adelmahmoudfawzy@gmail.com>
2025-01-22 14:17:33 +0200

    Remove debugging logs from main.cpp

--- design-patterns/observer-mediator/src/main.cpp
+++ design-patterns/observer-mediator/src/main.cpp
@@ -3,15 +3,9 @@
 #include <thread>
 int main()
 {
-    // App app;
-    // app.run();
-    std::cout << "Adssel" << std::endl;
-    fflush(stdout); // Force output to the terminal
-
+    App app;
+    app.run();
     std::this_thread::sleep_for(std::chrono::seconds(5));
-    std::cout << "Adel" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    std::cout << "Fawzy" << std::endl;
-    // std::cout << "app exiting" << std::endl;
+    std::cout << "App exiting" << std::endl;
     return 0;
 }
